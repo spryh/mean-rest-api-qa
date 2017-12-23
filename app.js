53c97ca2823a1a05b31aff2e81fa54e94182b682
Paul Scarpa <spryh@Bootstrap.home>
2017-12-23 00:00:37 -0800

    thru 2.1 - Middleware fun

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 'use strict'
 
 var express = require('express')
-
 var app = express()
+var jsonParser = require('body-parser').json
+
+var jsonCheck = function(req, res, next){
+    if(req.body){
+        console.log(`The sky is ${req.body.color}`)
+    } else {
+        console.log(`There is no body property on the request`)
+    }
+    next()
+}
+
+app.use(jsonCheck)
+app.use(jsonParser())
+app.use(jsonCheck)
 
 var port = process.env.PORT || 3000
-
-// app.use((request, response, next)=>{
-//     //console.log('First piece of middleware.')
-//     request.myMessage = 'Hello Middleware #2!'
-//     next()
-// })
-
-// app.use((request, response, next)=>{
-//     //console.log(`Second piece of middleware, ID: ${request.params.id}`)
-//     console.log(request.myMessage)
-//     next()
-// })
-
-app.use((request, response, next)=>{
-    console.log(`The leaves on the trees are ${request.query.color}`)
-    next()
-})
 
 app.listen(port, () => console.log(`Express Server listening on port ${port}`))
